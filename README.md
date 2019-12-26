df7e53792dede3788f2b031e9334187c5372e570
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2019-12-26 17:08:25 +0200

    First Implementation of Redis caching Server
    Problems:
    - Caching code isn't easily reusable anywhere else in the codebase
    - Cached values never expire
    - Cache keys wont work when we introduce other collections or query options

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AdvancedNodeStarter
-Starting project for a course on Advanced Node @ Udemy
+Mongo DB creates an index on ID for every collection, so when we do a search for a document in the collection, and that search is made using ID, the search gets really fast using that Index.
+
+But if we want to make a search using title for e.x., then the search has to be done in every document in that collection(which is the default behaviour of MongoDB for non Indexed fields), to find which document(s) satisfy the rule of our search. That makes the query ultra expensive.
+
+Two solutions:
+1) Add indexes for every element we search in that collection. This is not so good solution because adding indexes slows down the write speed to DB.
+2) Add a Cache Server which will check if that query has been executed. If no, query to MongoDB will be done and then the results of that query will be saved to Cache Server. If yes, results from cache server will be returned to application, without querying the Database.
+
+We will go with solution 2, Adding a Redis cache server is the proper solution to the problem.
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const redis = require('redis');
+const util = require('util');
 
 const Blog = mongoose.model('Blog');
+const redisUrl = 'redis://127.0.0.1:6379';
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
@@ -14,9 +17,23 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const userId = req.user.id;
+    const client = redis.createClient(redisUrl);
+    // Using util.promisify to promisify client.get function so that we can handle this function like promise and not callbacks
+    // assigning promisified function to current client.get, makes the function to return a promise 
+    client.get = util.promisify(client.get);
+    // Do we have any cached data in redis related to this query?
+    const cachedBlogs = await client.get(userId);
+    // If yes, then respond to the request right away and return
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    // If no, we need to respond to request and update our cache to store the data
+    console.log('SERVING FROM MONGODB');
     const blogs = await Blog.find({ _user: req.user.id });
-
     res.send(blogs);
+    client.set(userId, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
