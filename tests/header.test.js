cc9634d07d7b19a526d7f4f82e6b35ca6d708a9d
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2020-03-03 17:24:21 +0200

    - Created test that redirects to login screen and checks if login starts auth flow - Created test that creates a session and session.sig cookie based on userId and adds that cookie to the headless chromium browser so that we can make authenticated requests

--- tests/header.test.js
+++ tests/header.test.js
@@ -21,7 +21,34 @@ afterEach(async () => {
  * the first argument is the description of the test
  * the second argument is a function that is called for testing
  */
-test('We can launch a browser', async () => {
+test('Header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
+});
+
+test('Clicking login starts auth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/)
+});
+
+test('When signed in, shows logout button', async () => {
+  const id = '5e020f4cfec2c5431d52f4d8';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign(`session=${sessionString}`);
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+  await page.waitFor('ul.right li:nth-child(2) a');
+  const logoutButton = await page.$eval('ul.right li:nth-child(2) a', el => el.innerHTML);
+  expect(logoutButton).toEqual('Logout')
 });
