47cba466e5028ff6472e079480ab5236a81b65ca
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2020-03-03 13:00:27 +0200

    Adds more testing

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,9 +1,27 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+// Runs before each test ends
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+// Runs after each test ends
+afterEach(async () => {
+  await browser.close();
+});
+
 /**
  * Define a new function
  * the first argument is the description of the test
  * the second argument is a function that is called for testing
  */
-test('Adds two numbers', () => {
-  const sum = 1 + 2;
-  expect(sum).toEqual(3);
+test('We can launch a browser', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
